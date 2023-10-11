8346f2ccbec973e914196d4b568d65f82144f398
proximalbeet <nake73a@gmail.com>
2023-10-11 09:56:56 -0400

    Add project files.

---
+++ Match Game/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Match_Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+        private void SetUpGame()
+        {
+            List<string> FoodEmoji = new List<string>()
+            {
+                "🍕","🍕",
+                "🍔","🍔",
+                "🍟","🍟",
+                "🌭","🌭",
+                "🍿","🍿",
+                "🥞","🥞",
+                "🥪","🥪",
+                "🌯","🌯",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>()) 
+            {
+                int index = random.Next(FoodEmoji.Count);
+                string nextEmoji = FoodEmoji[index];
+                textBlock.Text = nextEmoji;
+                FoodEmoji.RemoveAt(index);
+
+            }
+        }
+    }
+}
